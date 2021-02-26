f20896bb36e2826e6c4e74ce6d4d857477d755f4
zemdom <zembrzuskadom@gmail.com>
2021-02-26 13:27:50 +0100

    Add --pmem-ratio option for runtest - to test ratio configuration run: ./runtest --pmem-ratio - allows to run test for configuration including ratio policy - update default.conf with following values:     memory-alloc-policy ratio     dram-pmem-ratio 1 3     initial-dynamic-threshold 64     memory-ratio-check-period 100     hashtable-on-dram yes

--- tests/test_helper.tcl
+++ tests/test_helper.tcl
@@ -80,6 +80,7 @@ set ::baseport 21111; # initial port for spawned redis servers
 set ::portcount 8000; # we don't wanna use more than 10000 to avoid collision with cluster bus ports
 set ::traceleaks 0
 set ::valgrind 0
+set ::pmem_ratio_test 0
 set ::durable 0
 set ::tls 0
 set ::stack_logging 0
@@ -524,6 +525,7 @@ proc send_data_packet {fd status data} {
 proc print_help_screen {} {
     puts [join {
         "--valgrind         Run the test over valgrind."
+        "--pmem-ratio       Run the tests with pmem-ratio variant."
         "--durable          suppress test crashes and keep running"
         "--stack-logging    Enable OSX leaks/malloc stack logging."
         "--accurate         Run slow randomized tests for more iterations."
@@ -583,6 +585,8 @@ for {set j 0} {$j < [llength $argv]} {incr j} {
         incr j
     } elseif {$opt eq {--valgrind}} {
         set ::valgrind 1
+    } elseif {$opt eq {--pmem-ratio}} {
+        set ::pmem_ratio_test 1
     } elseif {$opt eq {--stack-logging}} {
         if {[string match {*Darwin*} [exec uname -a]]} {
             set ::stack_logging 1
